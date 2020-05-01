3ae06c23902d190bea7f546a44d440c379c654df
dmdekf <dmdekf@gmail.com>
2020-05-01 10:03:51 +0900

    Add accounts index.html

--- instak/accounts/views.py
+++ instak/accounts/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
-from .models import User
+from django.shortcuts import render, redirect, get_list_or_404
+from django.contrib.auth import get_user_model
+User = get_user_model()
 # Create your views here.
 
 
 def index(request):
-    pass
+    users = get_list_or_404(User)
+    context = {
+        'users': users
+    }
+    return render(request, 'accounts/index.html', context)
